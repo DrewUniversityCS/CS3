a4c56bace5a228e47417d2df2c0b938b072e2184
NullDefault <davisha999@gmail.com>
2021-01-28 13:58:39 -0500

    some temp styles
    Former-commit-id: dfdb61ae772739516b21cde6aed97d2a00530689

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
