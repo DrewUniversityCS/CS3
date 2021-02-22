ab70a4686498188060290d8b1debc58302311424
NullDefault <davisha999@gmail.com>
2021-02-22 16:01:55 -0500

    fleshed out more models

--- database/models/structural_models.py
+++ database/models/structural_models.py
@@ -6,12 +6,21 @@ class Department(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False, primary_key=True)
     department_head = models.ForeignKey("database.Teacher", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name + ": " + self.abbreviation
+
 
 class Building(models.Model):
     name = models.CharField(max_length=256, blank=False, null=False, primary_key=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
     number = models.IntegerField(blank=True, null=True)
     building = models.ForeignKey("database.Building", on_delete=models.CASCADE)
     max_occupancy = models.IntegerField(blank=False, null=True)
+
+    def __str__(self):
+        return self.building.name + " " + str(self.number)
--- database/models/user_models.py
+++ database/models/user_models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
-from database.reliability.enums import YEAR_STANDING_CHOICES
+from database.reliability.enums import YEAR_IN_SCHOOL_CHOICES
 from database.reliability.validators import student_id_validator
 
 
 class Student(models.Model):
     user = models.ForeignKey("accounts.BaseUser", on_delete=models.CASCADE, blank=False, null=False)
     student_id = models.IntegerField(unique=True, validators=[student_id_validator])
-    class_standing = models.CharField(max_length=2, choices=YEAR_STANDING_CHOICES)
+    class_standing = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES,
+                                      default=YEAR_IN_SCHOOL_CHOICES.FRESHMAN)
 
     def __str__(self):
         return self.user.get_full_name() + ', ' + self.class_standing + ' : ' + str(self.student_id)
+
+    def is_upperclassman(self):
+        return self.class_standing in {YEAR_IN_SCHOOL_CHOICES.JUNIOR, YEAR_IN_SCHOOL_CHOICES.SENIOR}
+
+    def is_senior(self):
+        return self.class_standing == YEAR_IN_SCHOOL_CHOICES.SENIOR
 
 
 class Teacher(models.Model):
