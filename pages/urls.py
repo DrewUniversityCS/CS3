e1df8a9fd865bf83164988598d650dc037733f73
malminawi <mahmoud@alminawi.org>
2021-02-24 00:46:34 -0500

    refactoring

--- pages/urls.py
+++ pages/urls.py
@@ -7,7 +7,7 @@ from .views import HomePageView, AboutPageView, InviteView
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('invite/', InviteView.as_view(), name='invite'),
-    path('invite-success/', TemplateView.as_view(template_name='account/newAdminRegistrationSuccess.html'),
+    path('invite-success/', TemplateView.as_view(template_name='account/new_admin_registration_success.html'),
          name='invite-success'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('generate-schedule', page_views.GenerateScheduleView.as_view(), name='generate-schedule'),
