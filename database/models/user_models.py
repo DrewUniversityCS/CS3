96e397d8fa39931d69eb4ce45c316afc0dfffd9e
David Nesterov-Rappoport <48499573+NullDefault@users.noreply.github.com>
2021-02-24 17:18:55 -0500

    hotfix (#10)

--- database/models/user_models.py
+++ database/models/user_models.py
@@ -10,8 +10,7 @@ class Student(models.Model):
     """
     user = models.ForeignKey("accounts.BaseUser", on_delete=models.CASCADE, blank=False, null=False)
     student_id = models.IntegerField(unique=True, validators=[student_id_validator])
-    class_standing = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES,
-                                      default=YEAR_IN_SCHOOL_CHOICES.FRESHMAN)
+    class_standing = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES)
 
     def __str__(self):
         return self.user.get_full_name() + ', ' + self.class_standing + ' : ' + str(self.student_id)
