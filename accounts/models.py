b1cc1ad2750a59d3e59c6cd5b359c22bce2813dc
David Nesterov-Rappoport <48499573+NullDefault@users.noreply.github.com>
2021-03-01 15:10:34 -0500

    Database implementation (#11)
    * hotfix
    
    * relationship time! wrote my initial vision for preferences between models
    
    * massive database design improvements
    
    * uploaded relational schema
    
    * fixes and migrations
    
    * removed unnecessary folder nesting
    
    * forgot to fix imports after folder change
    
    Former-commit-id: 6661ae01a4dad3676a2bd5c35823de98b3995813

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class BaseUser(AbstractUser):
-    pass
+
+    class_preferences = models.ManyToManyField("database.Course", through="database.CoursePreference")
+    room_preferences = models.ManyToManyField("database.Room", through="database.RoomPreference")
+    time_preferences = models.ManyToManyField("database.TimeBlock", through="database.TimePreference")
 
     def __str__(self):
         return self.email
