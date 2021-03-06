3a447060558a1081d0376aca1f29adf216c0da3c
malminawi <mahmoud@alminawi.org>
2021-03-06 18:24:35 -0500

    creating dynamic views to test

--- database/urls.py
+++ database/urls.py
@@ -1,28 +1,28 @@
 from django.urls import path
 
-from database.views import department_crud, room_crud, building_crud, student_crud, teacher_crud, course_crud, \
-    section_crud, schedule_crud, timeblock_crud, course_preference_crud, overlap_preference_crud, room_preference_crud, \
-    timeblock_preference_crud, registration_crud
+from database.views import CrudView, CrudDeleteView
 
 urlpatterns = [
     # User Models
-    path('crud/students', student_crud),
-    path('crud/teachers', teacher_crud),
-    # Structural Models
-    path('crud/departments', department_crud),
-    path('crud/rooms', room_crud),
-    path('crud/buildings', building_crud),
-    # Schedule Models
-    path('crud/courses', course_crud),
-    path('crud/sections', section_crud),
-    path('crud/schedules', schedule_crud),
-    path('crud/timeblocks', timeblock_crud),
-    # Relationships
-    path('crud/course-preferences', course_preference_crud),
-    path('crud/overlap-preferences', overlap_preference_crud),
-    path('crud/room-preferences', room_preference_crud),
-    path('crud/timeblock-preferences', timeblock_preference_crud),
-    path('crud/registrations', registration_crud),
+    path('crud/<slug:model>/', CrudView.as_view(), name='crud_model'),
+    path('crud-delete/<slug:model>/<slug:id>/', CrudDeleteView.as_view(), name='crud_delete'),
+    # path('crud/students', student_crud),
+    # path('crud/teachers', teacher_crud),
+    # # Structural Models
+    # path('crud/departments', department_crud),
+    # path('crud/rooms', room_crud),
+    # path('crud/buildings', building_crud),
+    # # Schedule Models
+    # path('crud/courses', course_crud),
+    # path('crud/sections', section_crud),
+    # path('crud/schedules', schedule_crud),
+    # path('crud/timeblocks', timeblock_crud),
+    # # Relationships
+    # path('crud/course-preferences', course_preference_crud),
+    # path('crud/overlap-preferences', overlap_preference_crud),
+    # path('crud/room-preferences', room_preference_crud),
+    # path('crud/timeblock-preferences', timeblock_preference_crud),
+    # path('crud/registrations', registration_crud),
 ]
 
 app_name = 'database'
--- database/views.py
+++ database/views.py
@@ -1,118 +1,178 @@
+from django.http import Http404
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-
-from database.forms import DepartmentForm, RoomForm, BuildingForm, StudentForm, TeacherForm, CourseForm, SectionForm, \
-    ScheduleForm, TimeblockForm, CoursePreferenceForm, OverlapPreference, OverlapPreferenceForm, RoomPreferenceForm, \
-    TimeblockPreferenceForm, RegistrationForm
-from database.models.relationships import CoursePreference, RoomPreference, TimeblockPreference, Registration
+from django.views.generic import ListView
+from database.forms import get_dynamic_model_form
+from database.models.relationships import CoursePreference, RoomPreference, TimeblockPreference, Registration, \
+    OverlapPreference
 from database.models.schedule_models import Course, Section, Schedule, Timeblock
 from database.models.structural_models import Department, Room, Building
 from database.models.user_models import Student, Teacher
+from django.views.generic.edit import FormView, DeleteView
+
+# from database.forms import DepartmentForm, RoomForm, BuildingForm, StudentForm, TeacherForm, CourseForm, SectionForm, \
+#     ScheduleForm, TimeblockForm, CoursePreferenceForm, OverlapPreference, OverlapPreferenceForm, RoomPreferenceForm, \
+#     TimeblockPreferenceForm, RegistrationForm
+# from database.models.relationships import CoursePreference, RoomPreference, TimeblockPreference, Registration
+# from database.models.schedule_models import Course, Section, Schedule, Timeblock
+# from database.models.structural_models import Department, Room, Building
+# from database.models.user_models import Student, Teacher
 
 # User Model Views
-
-
-def student_crud(request):
+class DynamicModelMixin(object):
+    dynamic_model = None
+    dynamic_model_name = None
+    model_map_dict = {
+        'students': Student,
+        'teachers': Teacher,
+        'registrations': Registration,
+        'course-preferences': CoursePreference,
+        'overlap-preferences': OverlapPreference,
+        'room-preferences': RoomPreference,
+        'timeblock-preferences': TimeblockPreference,
+        'departments': Department,
+        'buildings': Building,
+        'rooms': Room,
+        'schedules': Schedule,
+        'courses': Course,
+        'sections': Section,
+        'timeblocks': Timeblock,
+    }
+    def dispatch(self, request, *args, **kwargs):
+        self.dynamic_model_name = kwargs.get('model')
+        dynamic_model = self.model_map_dict.get(self.dynamic_model_name , None)
+        if dynamic_model:
+            self.dynamic_model = dynamic_model
+            return super().dispatch(request, args, kwargs)
+        raise Http404
+class CrudDeleteView(DynamicModelMixin, DeleteView):
+    template_name = "crud/generic_delete_view.html"
+    def get_object(self):
+        return get_object_or_404(self.dynamic_model, pk=self.kwargs.get('id'))
+    def get_success_url(self):
+        return reverse('database:crud_model', kwargs={'model': self.dynamic_model_name})
+class CrudView(DynamicModelMixin, FormView):
     template_name = "crud/generic_crud_view.html"
-    all_objects = Student.objects.all()
-    form = StudentForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
+    context_object_name = 'all_objects'
+    def get_form_class(self):
+        return get_dynamic_model_form(self.dynamic_model)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'all_objects': self.dynamic_model.objects.all(),
+            'dynamic_model_name': self.dynamic_model_name
+        })
+        return context
+    def get_success_url(self):
+        return self.request.path
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
-def teacher_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Teacher.objects.all()
-    form = TeacherForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-# Structural Model Views
-
-
-def department_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Department.objects.all()
-    form = DepartmentForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def room_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Room.objects.all()
-    form = RoomForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def building_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Building.objects.all()
-    form = BuildingForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-# Schedule Model Views
-
-
-def course_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Course.objects.all()
-    form = CourseForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def section_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Section.objects.all()
-    form = SectionForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def schedule_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Schedule.objects.all()
-    form = ScheduleForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def timeblock_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Timeblock.objects.all()
-    form = TimeblockForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-# Relationship Model Views
-
-
-def course_preference_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = CoursePreference.objects.all()
-    form = CoursePreferenceForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def overlap_preference_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = OverlapPreference.objects.all()
-    form = OverlapPreferenceForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def room_preference_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = RoomPreference.objects.all()
-    form = RoomPreferenceForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def timeblock_preference_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = TimeblockPreference.objects.all()
-    form = TimeblockPreferenceForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
-
-
-def registration_crud(request):
-    template_name = "crud/generic_crud_view.html"
-    all_objects = Registration.objects.all()
-    form = RegistrationForm()
-    return render(request, template_name, {"all_objects": all_objects, "form": form})
+# def student_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Student.objects.all()
+#     form = StudentForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def teacher_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Teacher.objects.all()
+#     form = TeacherForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# # Structural Model Views
+#
+#
+# def department_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Department.objects.all()
+#     form = DepartmentForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def room_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Room.objects.all()
+#     form = RoomForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def building_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Building.objects.all()
+#     form = BuildingForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# # Schedule Model Views
+#
+#
+# def course_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Course.objects.all()
+#     form = CourseForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def section_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Section.objects.all()
+#     form = SectionForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def schedule_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Schedule.objects.all()
+#     form = ScheduleForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def timeblock_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Timeblock.objects.all()
+#     form = TimeblockForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# # Relationship Model Views
+#
+#
+# def course_preference_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = CoursePreference.objects.all()
+#     form = CoursePreferenceForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def overlap_preference_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = OverlapPreference.objects.all()
+#     form = OverlapPreferenceForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def room_preference_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = RoomPreference.objects.all()
+#     form = RoomPreferenceForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def timeblock_preference_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = TimeblockPreference.objects.all()
+#     form = TimeblockPreferenceForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
+#
+#
+# def registration_crud(request):
+#     template_name = "crud/generic_crud_view.html"
+#     all_objects = Registration.objects.all()
+#     form = RegistrationForm()
+#     return render(request, template_name, {"all_objects": all_objects, "form": form})
