91bd5b8ea72c8d49fc870015e26956cccb8d1c5b
malminawi <mahmoud@alminawi.org>
2021-03-06 18:18:32 -0500

    creating dynamic forms to test

--- database/forms.py
+++ database/forms.py
@@ -21,95 +21,107 @@ class CrispyModelForm(ModelForm):
         self.helper.layout.append(Submit('save', 'save'))
 
 
+def get_dynamic_model_form(dynamic_model):
+    """
+    Return a form class based on dynamic_model
+    """
+    class DynamicModelForm(CrispyModelForm):
+        class Meta:
+            model = dynamic_model
+            fields = "__all__"
+    return DynamicModelForm
+
 # User Model Forms
 
-class StudentForm(CrispyModelForm):
-    class Meta:
-        model = Student
-        fields = "__all__"
 
 
-class TeacherForm(CrispyModelForm):
-    class Meta:
-        model = Teacher
-        fields = "__all__"
-
-# Structural Model Forms
-
-
-class DepartmentForm(CrispyModelForm):
-    class Meta:
-        model = Department
-        fields = "__all__"
-
-
-class BuildingForm(CrispyModelForm):
-    class Meta:
-        model = Building
-        fields = "__all__"
-
-
-class RoomForm(CrispyModelForm):
-    class Meta:
-        model = Room
-        fields = "__all__"
-
-
-# Schedule Model Forms
-
-
-class CourseForm(CrispyModelForm):
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
-class SectionForm(CrispyModelForm):
-    class Meta:
-        model = Section
-        fields = "__all__"
-
-
-class ScheduleForm(CrispyModelForm):
-    class Meta:
-        model = Schedule
-        fields = "__all__"
-
-
-class TimeblockForm(CrispyModelForm):
-    class Meta:
-        model = Timeblock
-        fields = "__all__"
-
-
-# Relationship Model Forms
-
-
-class CoursePreferenceForm(CrispyModelForm):
-    class Meta:
-        model = CoursePreference
-        fields = "__all__"
-
-
-class OverlapPreferenceForm(CrispyModelForm):
-    class Meta:
-        model = OverlapPreference
-        fields = "__all__"
-
-
-class RoomPreferenceForm(CrispyModelForm):
-    class Meta:
-        model = RoomPreference
-        fields = "__all__"
-
-
-class TimeblockPreferenceForm(CrispyModelForm):
-    class Meta:
-        model = TimeblockPreference
-        fields = "__all__"
-
-
-class RegistrationForm(CrispyModelForm):
-    class Meta:
-        model = Registration
-        fields = "__all__"
+# class StudentForm(CrispyModelForm):
+#     class Meta:
+#         model = Student
+#         fields = "__all__"
+#
+#
+# class TeacherForm(CrispyModelForm):
+#     class Meta:
+#         model = Teacher
+#         fields = "__all__"
+#
+# # Structural Model Forms
+#
+#
+# class DepartmentForm(CrispyModelForm):
+#     class Meta:
+#         model = Department
+#         fields = "__all__"
+#
+#
+# class BuildingForm(CrispyModelForm):
+#     class Meta:
+#         model = Building
+#         fields = "__all__"
+#
+#
+# class RoomForm(CrispyModelForm):
+#     class Meta:
+#         model = Room
+#         fields = "__all__"
+#
+#
+# # Schedule Model Forms
+#
+#
+# class CourseForm(CrispyModelForm):
+#     class Meta:
+#         model = Course
+#         fields = "__all__"
+#
+#
+# class SectionForm(CrispyModelForm):
+#     class Meta:
+#         model = Section
+#         fields = "__all__"
+#
+#
+# class ScheduleForm(CrispyModelForm):
+#     class Meta:
+#         model = Schedule
+#         fields = "__all__"
+#
+#
+# class TimeblockForm(CrispyModelForm):
+#     class Meta:
+#         model = Timeblock
+#         fields = "__all__"
+#
+#
+# # Relationship Model Forms
+#
+#
+# class CoursePreferenceForm(CrispyModelForm):
+#     class Meta:
+#         model = CoursePreference
+#         fields = "__all__"
+#
+#
+# class OverlapPreferenceForm(CrispyModelForm):
+#     class Meta:
+#         model = OverlapPreference
+#         fields = "__all__"
+#
+#
+# class RoomPreferenceForm(CrispyModelForm):
+#     class Meta:
+#         model = RoomPreference
+#         fields = "__all__"
+#
+#
+# class TimeblockPreferenceForm(CrispyModelForm):
+#     class Meta:
+#         model = TimeblockPreference
+#         fields = "__all__"
+#
+#
+# class RegistrationForm(CrispyModelForm):
+#     class Meta:
+#         model = Registration
+#         fields = "__all__"
