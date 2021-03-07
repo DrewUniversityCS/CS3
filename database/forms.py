676018754feefd5ff440b41ec2250b8582e35947
NullDefault <davisha999@gmail.com>
2021-03-07 16:44:49 -0500

    cleaning up code for merging into main

--- database/forms.py
+++ database/forms.py
@@ -2,12 +2,6 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.forms import ModelForm
 
-from database.models.relationships import CoursePreference, RoomPreference, TimeblockPreference, Registration, \
-    OverlapPreference
-from database.models.schedule_models import Course, Section, Schedule, Timeblock
-from database.models.structural_models import Department, Room, Building
-from database.models.user_models import Student, Teacher
-
 
 class CrispyModelForm(ModelForm):
     def __init__(self, *args, **kwargs):
@@ -25,103 +19,10 @@ def get_dynamic_model_form(dynamic_model):
     """
     Return a form class based on dynamic_model
     """
+
     class DynamicModelForm(CrispyModelForm):
         class Meta:
             model = dynamic_model
             fields = "__all__"
+
     return DynamicModelForm
-
-# User Model Forms
-
-
-
-# class StudentForm(CrispyModelForm):
-#     class Meta:
-#         model = Student
-#         fields = "__all__"
-#
-#
-# class TeacherForm(CrispyModelForm):
-#     class Meta:
-#         model = Teacher
-#         fields = "__all__"
-#
-# # Structural Model Forms
-#
-#
-# class DepartmentForm(CrispyModelForm):
-#     class Meta:
-#         model = Department
-#         fields = "__all__"
-#
-#
-# class BuildingForm(CrispyModelForm):
-#     class Meta:
-#         model = Building
-#         fields = "__all__"
-#
-#
-# class RoomForm(CrispyModelForm):
-#     class Meta:
-#         model = Room
-#         fields = "__all__"
-#
-#
-# # Schedule Model Forms
-#
-#
-# class CourseForm(CrispyModelForm):
-#     class Meta:
-#         model = Course
-#         fields = "__all__"
-#
-#
-# class SectionForm(CrispyModelForm):
-#     class Meta:
-#         model = Section
-#         fields = "__all__"
-#
-#
-# class ScheduleForm(CrispyModelForm):
-#     class Meta:
-#         model = Schedule
-#         fields = "__all__"
-#
-#
-# class TimeblockForm(CrispyModelForm):
-#     class Meta:
-#         model = Timeblock
-#         fields = "__all__"
-#
-#
-# # Relationship Model Forms
-#
-#
-# class CoursePreferenceForm(CrispyModelForm):
-#     class Meta:
-#         model = CoursePreference
-#         fields = "__all__"
-#
-#
-# class OverlapPreferenceForm(CrispyModelForm):
-#     class Meta:
-#         model = OverlapPreference
-#         fields = "__all__"
-#
-#
-# class RoomPreferenceForm(CrispyModelForm):
-#     class Meta:
-#         model = RoomPreference
-#         fields = "__all__"
-#
-#
-# class TimeblockPreferenceForm(CrispyModelForm):
-#     class Meta:
-#         model = TimeblockPreference
-#         fields = "__all__"
-#
-#
-# class RegistrationForm(CrispyModelForm):
-#     class Meta:
-#         model = Registration
-#         fields = "__all__"
--- database/urls.py
+++ database/urls.py
@@ -3,26 +3,8 @@ from django.urls import path
 from database.views import CrudView, CrudDeleteView
 
 urlpatterns = [
-    # User Models
     path('crud/<slug:model>/', CrudView.as_view(), name='crud_model'),
     path('crud-delete/<slug:model>/<slug:id>/', CrudDeleteView.as_view(), name='crud_delete'),
-    # path('crud/students', student_crud),
-    # path('crud/teachers', teacher_crud),
-    # # Structural Models
-    # path('crud/departments', department_crud),
-    # path('crud/rooms', room_crud),
-    # path('crud/buildings', building_crud),
-    # # Schedule Models
-    # path('crud/courses', course_crud),
-    # path('crud/sections', section_crud),
-    # path('crud/schedules', schedule_crud),
-    # path('crud/timeblocks', timeblock_crud),
-    # # Relationships
-    # path('crud/course-preferences', course_preference_crud),
-    # path('crud/overlap-preferences', overlap_preference_crud),
-    # path('crud/room-preferences', room_preference_crud),
-    # path('crud/timeblock-preferences', timeblock_preference_crud),
-    # path('crud/registrations', registration_crud),
 ]
 
 app_name = 'database'
