7071acf826647c4d841e4cf4dd790d113377551a
NullDefault <davisha999@gmail.com>
2021-03-09 16:52:06 -0500

    styling and background improvements

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -11,7 +11,12 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: theme => ({
+                'no-data': "url('/static/images/signal_heropattern.svg')",
+                'endlessclouds': "url('/static/images/endless-clouds_heropattern.svg')"
+            })
+        },
     },
     variants: {
         extend: {},
