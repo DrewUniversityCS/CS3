b7d0fe811e6ff6d9f6b202595c0ea4879ffac584
abax17 <78123718+abax17@users.noreply.github.com>
2021-03-09 12:08:56 -0500

    NavBar refactoring (#22)
    * NavBar refactoring
    
    * small edits & refactors
    
    Co-authored-by: NullDefault <davisha999@gmail.com>
    Former-commit-id: 0f05e41d4d183eb2a1ca9b9006c84ca156ca4fb2

--- pages/views.py
+++ pages/views.py
@@ -8,10 +8,6 @@ class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 
-class AboutPageView(TemplateView):
-    template_name = 'pages/about.html'
-
-
 class DocsView(LoginRequiredMixin, TemplateView):
     template_name = 'pages/docs.html'
 
