6931704bdec6bddd26b68ec600fdfc2e08b94d9b
NullDefault <davisha999@gmail.com>
2021-03-10 15:12:10 -0500

    updated pipfiles
    Former-commit-id: cd67182fafcee342812e17ed724173906bcb4e09

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,7 +7,7 @@ SECRET_KEY = '43)%4yx)aa@a=+_c(fn&kf3g29xax+=+a&key9i=!98zyim=8j'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
