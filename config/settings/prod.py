13acbf3cf8d326edca63242e3e68e9e8be69171e
NullDefault <davisha999@gmail.com>
2021-03-17 09:42:46 -0400

    trying stuff again

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,7 +12,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
