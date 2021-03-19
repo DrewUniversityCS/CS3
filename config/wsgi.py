be6db26511e3d6f74c7a0059b09ac01e36c3ff65
NullDefault <davisha999@gmail.com>
2021-03-18 20:13:14 -0400

    god pls work

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root='/staticfiles')
