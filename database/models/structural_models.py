6b0f07d354ae1ac0e15df8bc383e11e0f0077464
NullDefault <davisha999@gmail.com>
2021-04-05 12:51:15 -0400

    preference form phase 1
    Former-commit-id: 293b92ce9ef097dcfdfdc45be89989cdb35719b4

--- database/models/structural_models.py
+++ database/models/structural_models.py
@@ -45,22 +45,28 @@ class Preference(models.Model):
     weight = models.BooleanField()
 
     """
-    Possible Constraint Expressions:
+    Possible Preference Expressions:
         - Course / Other Course
             - Course overlaps and pre/co requisites are expressed this way.
         - Course / User 
             - Does a teacher / student want to teach / take the class.
         - Course / Timeblock
             - A course should / should not be offered during a timeblock.
-        - Course / Other Course / Timeblock
-            - The following two classes cannot be offered during the same timeblock.
         - User (Teacher) / Timeblock
             - The given user needs to / can't teach a class during the given timeblock.        
     """
-    course = models.ForeignKey("database.Course", on_delete=models.CASCADE, null=True, blank=True)
-    other_course = models.ForeignKey("database.Course", on_delete=models.CASCADE, related_name="other course+")
-    user = models.ForeignKey("accounts.BaseUser", on_delete=models.CASCADE, null=True, blank=True)
-    timeblock = models.ForeignKey("database.Timeblock", on_delete=models.CASCADE)
+    limit = models.Q(app_label='database', model='course') | \
+            models.Q(app_label='accounts', model='baseuser') | \
+            models.Q(app_label='database', model='timeblock')
+
+    object_1_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True,
+                                              related_name="object_1_content_type", limit_choices_to=limit)
+    object_1_id = models.PositiveIntegerField(null=True, blank=True)
+    object_1 = GenericForeignKey('object_1_content_type', 'object_1_id')
+    object_2_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True,
+                                              related_name="object_2_content_type", limit_choices_to=limit)
+    object_2_id = models.PositiveIntegerField(null=True, blank=True)
+    object_2 = GenericForeignKey('object_2_content_type', 'object_2_id')
 
     sets = GenericRelation(SetMembership, related_query_name='preference')
 
