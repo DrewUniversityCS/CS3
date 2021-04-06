c610b0f6f0862f5e951deee425dc6c6bef278c7d
NullDefault <davisha999@gmail.com>
2021-04-06 18:05:29 -0400

    data parsing foundations
    Former-commit-id: 49995e8674cee19bca66677bb8b0028d4194db35

--- dataingest/forms.py
+++ dataingest/forms.py
@@ -1,4 +1,39 @@
+from io import StringIO
+
+from django.core.exceptions import ValidationError
 from django.forms import Form, ChoiceField, FileField
+
+
+def validate_csv_columns(col_names, category):
+    if category == 'course':
+        required_course_fields = ['department', 'name', 'number']
+
+        for field in required_course_fields:
+            if field not in col_names:
+                print(field)
+                return False
+        return True
+
+    elif category == 'student':
+        required_student_fields = ['student_id', 'class_standing', 'first_name', 'last_name', 'email']
+
+        for field in required_student_fields:
+            if field not in col_names:
+                print(field)
+                return False
+        return True
+
+    elif category == 'preference':
+        required_preference_fields = ['object_1_content_type', 'object_1', 'object_2_content_type', 'object_2']
+
+        for field in required_preference_fields:
+            if field not in col_names:
+                print(field)
+                return False
+        return True
+
+    else:
+        return False
 
 
 class UploadCSVFileForm(Form):
@@ -8,3 +43,19 @@ class UploadCSVFileForm(Form):
         ('preference', "Preferences"),
     ), required=True)
     file = FileField(required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        file = cleaned_data['file']
+        category = cleaned_data['category']
+        if file:
+            data_set = file.read().decode('UTF-8')
+            r = StringIO(data_set)
+            col_names = next(r)
+
+            columns_are_valid = validate_csv_columns(col_names, category)
+
+            if not columns_are_valid:
+                raise ValidationError("File doesn't have the necessary fields")
+
+        return cleaned_data
---
+++ dataingest/logistics.py
@@ -0,0 +1,15 @@
+from database.models.schedule_models import Course
+from database.models.structural_models import Preference
+from database.models.user_models import Student
+
+
+def create_courses(reader):
+    return []
+
+
+def create_students(reader):
+    return []
+
+
+def create_preferences(reader):
+    return []
