49995e8674cee19bca66677bb8b0028d4194db35
NullDefault <davisha999@gmail.com>
2021-04-06 18:05:29 -0400

    data parsing foundations

--- dataingest/views.py
+++ dataingest/views.py
@@ -1,8 +1,12 @@
+from io import StringIO
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.serializers import serialize
 from django.http import HttpResponseRedirect
 from django.views.generic import FormView
 
 from dataingest.forms import UploadCSVFileForm
+from dataingest.logistics import create_courses, create_students, create_preferences
 
 
 class UploadCSVFileView(LoginRequiredMixin, FormView):
@@ -11,6 +15,20 @@ class UploadCSVFileView(LoginRequiredMixin, FormView):
     success_url = '/'
 
     def form_valid(self, form):
-        print(form.data['category'])
-        print(self.request.FILES['file'])
+        category = form.data['category']
+        file = self.request.FILES['file']
+
+        data_set = file.read().decode('UTF-8')
+        r = StringIO(data_set)
+
+        objects = []
+        if category == 'course':
+            objects = create_courses(r)
+        elif category == 'student':
+            objects = create_students(r)
+        elif category == 'preference':
+            objects = create_preferences(r)
+
+        self.request.session['objects'] = serialize('json', objects)
+
         return HttpResponseRedirect(self.get_success_url())
