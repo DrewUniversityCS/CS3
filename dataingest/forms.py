6e95f9c29b50f57943ec5e3d1f81af2eb5a30a09
Drew-hkna <lhenry1@drew.edu>
2021-04-14 13:08:34 -0400

    Validation

--- dataingest/forms.py
+++ dataingest/forms.py
@@ -50,10 +50,13 @@ class UploadCSVFileForm(Form):
         cleaned_data = super().clean()
         file = cleaned_data['file']
         category = cleaned_data['category']
+        course_array = ['department', 'name', 'number']
+        student_array = ['student_id', 'class_standing', 'first_name', 'last_name', 'email']
+        preference_array = ['object_1_content_type', 'object_1', 'object_2_content_type', 'object_2']
+        req_fields_cases = {'course': course_array,
+                            'student': student_array,
+                            'preference': preference_array}
 
-        req_fields_cases = {'course': 'department, name, number',
-                            'student': 'student_id, class_standing, first_name, last_name, email',
-                            'preference': 'object_1_content_type, object_1, object_2_content_type, object_2'}
         required_fields = req_fields_cases.get(category)
 
         if file:
@@ -63,10 +66,25 @@ class UploadCSVFileForm(Form):
 
             columns_are_valid = validate_csv_columns(col_names, category)
 
+            i = 0
+            wrong_cat_cols = []
+            wrong_cat_cols_index = []
+            for i in range(len(required_fields)):
+                if required_fields[i] in col_names:
+                    i = i + 1
+
+                elif required_fields[i] not in col_names:
+                    wrong_cat_cols.append(required_fields[i])
+                    wrong_cat_cols_index.append(i+1)
+                    i = i + 1
+
+            string_ints = [str(int) for int in wrong_cat_cols_index]
+            str_of_ints = ",".join(string_ints)
+
             if not columns_are_valid:
-                raise ValidationError(mark_safe("File doesn't have the necessary fields. You gave: " + col_names +
-                                                "<br />For " + category +
-                                                " these are the column names you need to have:"
-                                                + "<br />" + required_fields))
+                raise ValidationError(mark_safe("File doesn't have the necessary column names. You gave: " + col_names +
+                                                "<br />To correct this, change column number(s) "
+                                                + str_of_ints + " to "
+                                                + "\'" + ",".join(wrong_cat_cols) + "\'" + " in your csv!"))
 
         return cleaned_data
