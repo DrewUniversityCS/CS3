2faa7185997bef2da3c1e5208e236d110d315d9a
NullDefault <davisha999@gmail.com>
2021-04-20 12:35:30 -0400

    changed settings files

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,19 +4,3 @@ from .base import *
 DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-SECRET_KEY
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-
-import pymysql
-
-pymysql.version_info = (1, 4, 6, 'final', 0)
-pymysql.install_as_MySQLdb()
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '/cloudsql/rapid-league-302721:us-east1:my-sql-db',
-        'USER': 'django',
-        'PASSWORD': 'cs3',
-        'NAME': 'django_data'
-    }
-}
