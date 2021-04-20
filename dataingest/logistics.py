d38262a3c76b18e8f3bac326f4aa3e51dfc2773e
NullDefault <davisha999@gmail.com>
2021-04-20 18:41:06 -0400

    moved required fields elsewhere

--- dataingest/logistics.py
+++ dataingest/logistics.py
@@ -5,13 +5,21 @@ from database.models.structural_models import Preference
 from database.models.user_models import Student
 
 
+required_course_fields = ['department', 'name', 'number']
+required_student_fields = ['student_id', 'class_standing', 'first_name', 'last_name', 'email']
+required_preference_fields = ['object_1_content_type', 'object_1', 'object_2_content_type', 'object_2']
+
+
 def create_courses(reader):
     shipback = []
-    for row in reader[1:]:
+    for row in reader:
+        print(row)
         shipback.append(Course.objects.get_or_create(
-
+            department=row['department'],
+            name=row['name'],
+            number=row['number']
         ))
-    return []
+    return shipback
 
 
 def create_students(reader):
