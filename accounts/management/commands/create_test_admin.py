2aa0b1bb78551ae327a906731650bd6897ae87ce
NullDefault <davisha999@gmail.com>
2021-04-20 15:44:13 -0400

    migrating over useful changes from production branch

--- accounts/management/commands/create_test_admin.py
+++ accounts/management/commands/create_test_admin.py
@@ -12,7 +12,10 @@ class Command(BaseCommand):
         self._create_test_admin()
 
     def _create_test_admin(self):
-        BaseUser.objects.create_superuser('admin', 'admin@gmail.com', '123')
-        BaseUser.objects.filter(is_superuser=True).update(
-            first_name='Super', last_name='User'
-        )
+        try:
+            admin = BaseUser.objects.get(email='admin@gmail.com')
+        except BaseUser.DoesNotExist:
+            BaseUser.objects.create_superuser('admin', 'admin@gmail.com', '123')
+            BaseUser.objects.filter(is_superuser=True).update(
+                first_name='Super', last_name='User'
+            )
