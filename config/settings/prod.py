32cd2a0fa78eecd0c8fcd4c526172d350118a4fd
NullDefault <davisha999@gmail.com>
2021-04-20 13:59:24 -0400

    included django_heroku

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,13 +1,9 @@
 from .base import *
+import django_heroku
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-SECRET_KEY
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+django_heroku.settings(locals())
