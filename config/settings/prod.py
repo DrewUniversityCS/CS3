14e3251bcb2730caabe13eedc112b909e9899c5a
NullDefault <davisha999@gmail.com>
2021-04-20 12:57:04 -0400

    fixing db issue

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,10 @@ from .base import *
 DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-SECRET_KEY
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
