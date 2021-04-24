92e76ef1a9807681268c983c8c4cbd9ee027aec3
NullDefault <davisha999@gmail.com>
2021-04-24 17:14:12 -0400

    refactors

--- dataingest/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-models = []
-
-for model in models:
-    admin.site.register(model)
--- static/js/schedule-filter.js
+++ static/js/schedule-filter.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('.js_session_filter').forEach(item => {
   item.addEventListener('change', event => {
-      console.log('Hello')
     document.querySelectorAll(`.js_section_${event.target.id}`).forEach(item => {
         item.style.display = event.target.checked ? 'block' : 'none'
     })
