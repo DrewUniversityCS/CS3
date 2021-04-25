03dd8a68a2986964272fc229d37e61047d155308
Mahmoud Alminawi <mahmoud@alminawi.org>
2021-04-25 12:03:22 -0400

    spinner function

--- static/js/schedule-filter.js
+++ static/js/schedule-filter.js
@@ -6,3 +6,15 @@ document.querySelectorAll('.js_session_filter').forEach(item => {
   })
 })
 
+function loadEditForm(url, selector) {
+    console.log('Being Called')
+    var formDivs = document.querySelectorAll(selector)
+    console.log(formDivs[0].firstChild.tagName)
+    if (formDivs[0].firstChild.tagName == 'IMG' ) {
+        fetch(url).then(response => response.text()).then(data => {
+            formDivs.forEach(item => {
+                item.innerHTML = data;
+            })
+        });
+    }
+}
