6fc2380985209c980f4abf9e9fa0dab560b8e772
David Nesterov-Rappoport <48499573+NullDefault@users.noreply.github.com>
2021-05-04 14:54:25 -0400

    Heroku fix (#63)
    * attempting fix
    
    * another fix attempt
    
    * force commit?
    
    * god damn it why wont this work
    
    * will this break it?

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,7 +12,6 @@ ALLOWED_HOSTS = ['*']
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,8 +48,8 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
